323029bddf4b346e0731a53e8b6860fe13f5c918
Gianmarco Benigni <Gianmarco.Benigni@aesystech.it>
2020-02-20 17:15:27 +0100

    Aggiunta Film_component e details

---
+++ src/app/film/film.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-film',
+  templateUrl: './film.component.html',
+  styleUrls: ['./film.component.scss']
+})
+export class FilmComponent implements OnInit {
+
+  @Input() filmData;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
+
