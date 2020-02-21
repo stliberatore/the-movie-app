a32f2212406eb523ec374b774383c527a87bad01
Gianmarco Benigni <Gianmarco.Benigni@aesystech.it>
2020-02-21 14:15:56 +0100

    Film-Details

--- src/app/services/api-request.service.ts
+++ src/app/services/api-request.service.ts
@@ -13,27 +13,21 @@ export class ApiRequestService {
   private dashboardUrl =
     // tslint:disable-next-line: max-line-length
     'https://api.themoviedb.org/3/discover/movie?language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=';
-<<<<<<< HEAD
-  private filmDetailUrl = 'https://api.themoviedb.org/3/movie'
-=======
 
-<<<<<<< HEAD
+  private filmDetailUrl = 'https://api.themoviedb.org/3/movie'
+
+
   private backdropImageUrl = 'https://image.tmdb.org/t/p/w300';
 
->>>>>>> fix service "getFilmPreview", now it retrieves images
-=======
->>>>>>> fix services and slider bar
+
+
+
   private randomPageIndex = angularMath.getIntegerRandomRange(1, 500);
 
   constructor(private http: HttpClient) { }
 
   getFilmPreview(): Observable<FilmResponse> {
-<<<<<<< HEAD
-    return this.http.get<FilmResponse>( `${this.dashboardUrl}${this.randomPageIndex}`
-=======
-    return this.http.get<FilmResponse>(
-      `${this.dashboardUrl}${this.randomPageIndex}`
->>>>>>> fix service "getFilmPreview", now it retrieves images
+    return this.http.get<FilmResponse>(`${this.dashboardUrl}${this.randomPageIndex}`
     );
   }
   getDetail(id: string): Observable<FilmDetails[]> {
